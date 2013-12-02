60e0c967cd318c1bc337e445ba5a18775b0404f0
ruce2728 <bruce2728@gmail.com>
2013-12-02 19:51:01 +0000

    lights.msm8960

--- device.mk
+++ device.mk
@@ -29,7 +29,7 @@ PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 PRODUCT_PACKAGES := \
-	lights.mako
+	lights.msm8960
 
 PRODUCT_PACKAGES += \
     charger_res_images \
