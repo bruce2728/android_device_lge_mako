6dfbf8251abb2af58bfca806163d0409015ab2f3
ruce2728 <bruce2728@gmail.com>
2013-12-08 21:02:24 +0000

    revert sepolicy changes to avoid problems with lazy people flashing without wiping data

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,34 +110,28 @@ TARGET_PROVIDES_GPS_LOC_API := true
 
 TARGET_RELEASETOOLS_EXTENSIONS := device/lge/mako
 
-TARGET_SEPOLICY_NEW := true
-
-BOARD_SEPOLICY_DIRS += \
+BOARD_SEPOLICY_DIRS := \
        device/lge/mako/sepolicy
 
-BOARD_SEPOLICY_UNION += \
+BOARD_SEPOLICY_UNION := \
        app.te \
-       bluetooth_loader.te \
-       bridge.te \
-       camera.te \
-       conn_init.te \
+       bluetooth.te \
        device.te \
+       domain.te \
+       drmserver.te \
        file.te \
        file_contexts \
-       kickstart.te \
+       hci_init.te \
+       init_shell.te \
+       keystore.te \
        mediaserver.te \
-       mpdecision.te \
-       netmgrd.te \
-       qmux.te \
+       kickstart.te \
+       nfc.te \
        rild.te \
-       rmt.te \
-       sensors.te \
        surfaceflinger.te \
-       system_server.te \
-       tee.te \
-       te_macros \
-       thermald.te \
-       ueventd.te
+       system.te \
+       ueventd.te \
+       wpa.te
 
 
 BOARD_CHARGER_ENABLE_SUSPEND := true
